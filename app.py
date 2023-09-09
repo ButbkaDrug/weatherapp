1a321ddfad8098726b87f4fa818fa87eed306bf7
ButbkaDrug <butbkadrug@gmail.com>
2023-09-09 21:04:00 +0400

    Split code into functions. Added async functionality and support for quering multiple locations at once

--- app.py
+++ app.py
@@ -3,24 +3,14 @@
 import http.client
 import json
 import argparse
+import asyncio
+from typing import Union
 from os import getenv
+from sys import stderr, stdout
 
 API_KEY = getenv("WEATHER_API_KEY")
 
-def main():
-
-    parser = argparse.ArgumentParser(
-            description="Check the weather in the given city",
-            )
-    parser.add_argument("city")
-
-    args = parser.parse_args()
-
-    if not args.city:
-        city_name = input("Please, provide the name of the city: ")
-    else:
-        city_name = args.city
-
+async def query_api(query: str) -> str:
     conn = http.client.HTTPSConnection("weatherapi-com.p.rapidapi.com")
 
     headers = {
@@ -29,23 +19,73 @@ def main():
     }
 
 
-    conn.request("GET", f"/search.json?q={city_name}", headers=headers)
+    conn.request("GET", f"/current.json?q={query}", headers=headers)
 
     res = conn.getresponse()
-    data = res.read()
 
-    if len(data.decode("utf-8")) < 3:
-        print(f"I'm sorry. No city called {city_name} were found!")
-        exit()
+    data = json.loads(res.read())
+
+    return data
+
+def process_response( response:dict ) -> str:
+    output = ""
+
+
+    if response.get("error"):
+        code, message = response.get( "error" ).values()
+        print(f"ERROR {code}: {message}", file=stderr)
+        return
+
+    name = response.get( "location" ).get( "name" ).capitalize()
+    text = response.get( "current" ).get( "condition" ).get( "text" ).lower()
+
+    output = f"In { name } it is { text }"
+
+    return output
 
 
 
-    conn.request("GET", f"/current.json?q={city_name}", headers=headers)
+async def process_input(query:Union[ str, list[str] ]) -> list[str]:
+    output = []
 
-    res = conn.getresponse()
-    data = res.read()
 
-    print(json.loads(data))
+    queries = [query_api(q) for q in query]
+    responses = await asyncio.gather(*queries)
+
+    for response in responses:
+        data = process_response(response)
+
+        if data:
+            output.append(data)
+
+    return output
+
+
+
+
+async def main():
+
+    parser = argparse.ArgumentParser(
+            description="Check the weather in the given city",
+            )
+    parser.add_argument("city", nargs="+")
+    parser.add_argument("-o", "--output", action="store")
+
+    args = parser.parse_args()
+
+    city_name = args.city
+
+
+    output = await process_input(city_name)
+
+
+    if args.output:
+        with open(args.output, "w") as file:
+            file.writelines(output)
+    else:
+        for line in output:
+            print(line, file=stdout)
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run( main() )
